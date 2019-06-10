6b8726a7e36bf5dee366182a2beebf9b6b6d33ff
Jonathan Morales <jonmorazav@yahoo.com>
2019-06-10 18:41:14 -0400

    better ie detection and url validation

--- src/ie.js
+++ src/ie.js
@@ -1,5 +1,19 @@
 const ua = window.navigator.userAgent;
 const msie = ua.indexOf("MSIE ");
-if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./)) {
+let ie = false;
+if (msie > 0) {
+    // IE 10 or older => return version number
+    ie = true;
+}
+const trident = ua.indexOf('Trident/');
+if (trident > 0) {
+    // IE 11 => return version number
+    ie = true;
+}
+var edge = ua.indexOf('Edge/');
+if (edge > 0) {
+    ie = true;
+}
+if(ie){
     document.write("<center><h1>Sorry. IE is not supported at this time. Please download chrome to view this site. <a href='https://www.google.com/chrome'>https://www.google.com/chrome</a> </h1> </center><br/><iframe align='middle' width='100%' height='90%' src='https://www.google.com/chrome'></iframe>")
 }
